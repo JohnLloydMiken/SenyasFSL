1989215e9997a88c442b2b0ce1e24be3b4397615
John Lloyd Miken <mikenjohnlloyd@gmail.com>
2025-08-04 23:26:58 +0800

    Started the multiple choice game

--- SenyasFSL_Mobile/app/levels/[levelId].tsx
+++ SenyasFSL_Mobile/app/levels/[levelId].tsx
@@ -1,56 +1,23 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import LevelBg from "@/assets/svgs/LevelBG.svg";
+
 import React, { useState } from "react";
-import { useVideoPlayer, VideoView } from "expo-video";
-import { useLocalSearchParams } from "expo-router";
-import LevelContentBtn, {
-  MultipleChoiceBTN,
-} from "@/components/LevelContentBtn";
+import LearnASign from "@/components/Game_Modes/LearnASign";
+import MultipleChoice from "@/components/Game_Modes/MultipleChoice";
 export default function LevelContent() {
-  const [isClicked, setIsClicked] = useState(false);
-  const videoSource = require("@/assets/videos/FSL_A.mp4");
-  const { levelId } = useLocalSearchParams();
-  const player = useVideoPlayer(videoSource, (player) => {
-    player.loop = true;
-    player.muted = true; // Optional: mute the video
-    player.pause();
-  });
+ 
   return (
     <View className="flex-1  relative bg-white">
-      <Text className="text-center text-2xl md:text-3xl font-PoppinsBold my-2">Learn A New Sign</Text>
-      <View className="w-full h-[30%] relative -top-1">
-        <VideoView
-          style={{ width: "100%", height: "100%" }}
-          player={player}
-          allowsFullscreen={false}
-          allowsPictureInPicture={false}
-          nativeControls={false}
+        <MultipleChoice 
+        videoUrl={require('@/assets/videos/FSL_A.mp4')}  
+        title="Choice the right Sign"
+        correctAnswer="Letter A"
+        optionOne="Letter A"
+        optionOneFil="Letrang A"
+        optoptionTwo="Letter B"
+        optionTwoFil="Letrang B"
         />
-      </View>
-
-      <View className="w-11/12 mx-auto">
-        <MultipleChoiceBTN
-          EnglishText="Letter A"
-          FilipinoText= {`"Letrang A"`}
-          onPress={() => {
-            player.play();
-            setIsClicked(!isClicked);
-
-            if(isClicked){
-              player.pause()
-            }
-          }}
-          clicked={isClicked}
-        />
-      </View>
-
-      <View className="absolute bottom-16 w-56 md:w-64 left-1/2 -translate-x-1/2 z-50">
-        <LevelContentBtn text="Next" onPress={() => console.log("fwfewf")} />
-      </View>
-
-      <View className="absolute w-full bottom-0 z-10">
-        <LevelBg />
-      </View>
+        
+      
     </View>
   );
 }
