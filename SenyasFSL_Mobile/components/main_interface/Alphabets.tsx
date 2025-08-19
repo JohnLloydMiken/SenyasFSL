dc1cb28c3eca876769aa25d7c5e98c67a29acb18
John Lloyd Miken <mikenjohnlloyd@gmail.com>
2025-08-19 22:13:12 +0800

    Finish the dictionary Feature, only polish needed

--- SenyasFSL_Mobile/components/main_interface/Alphabets.tsx
+++ SenyasFSL_Mobile/components/main_interface/Alphabets.tsx
@@ -1,31 +1,33 @@
 import { View, Text , FlatList, TouchableOpacity, ScrollView} from 'react-native'
 import React from 'react'
 import Categories from '@/json_files/Categories.json'
-const Alphabets = () => {
-  const data = Categories[0].content?.letter
-  return (
-    <ScrollView style={{
-      flex: 1,
-      padding: 8,
-      
-    }}>
-    
-    {data?.map((item, index) =>{
-      return(
-        <TouchableOpacity key={index} className='w-11/12 border border-[#F7D674] rounded-full mx-auto p-2 my-3 flex-col justify-center items-center'>
-          <Text className='font-PoppinsBold text-2xl md:text-3xl text-[#8B8B8B]'>
-            {item}
 
+interface Props {
+  onPress: (letter: string, letterFil: string) => void;
+}
+
+const Alphabets: React.FC<Props> = ({ onPress }) => {
+  const data = Categories[0].content?.letter;
+  const dataFil = Categories[0].content?.letterFil;
+
+  return (
+    <ScrollView style={{ flex: 1, padding: 8 }}>
+      {data?.map((item, index) => (
+        <TouchableOpacity
+          key={index}
+          className="w-11/12 border border-[#F7D674] rounded-full mx-auto p-2 my-3 flex-col justify-center items-center"
+          onPress={() => onPress(item, dataFil?.[index] || "")}
+        >
+          <Text className="font-PoppinsBold text-2xl md:text-3xl text-[#8B8B8B]">
+            {item}
           </Text>
-          <Text className='text-[#8B8B8B] font-PoppinsRegular text-lg md:text-xl'>
-            {`"${Categories[0].content?.letterFil[index]}"`}
+          <Text className="text-[#8B8B8B] font-PoppinsRegular text-lg md:text-xl">
+            {`"${dataFil?.[index]}"`}
           </Text>
         </TouchableOpacity>
-      )
-
-    })}
+      ))}
     </ScrollView>
-  )
-}
+  );
+};
 
 export default Alphabets
